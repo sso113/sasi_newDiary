4d16c2a351df56aee2c7de205241561931ecb790
소영 <marbleksy@gmail.com>
2023-05-05 03:31:18 +0900

    To Do List commit

--- 0406/variables.js
+++ 0406/variables.js
@@ -1,5 +1,6 @@
 let el = document.getElementById( 'birthday');
-let elResult = document.getElementById('submit');
+let elResult = document.getElementById('result');
+let elSubmit = document.getElementById('submit');
 
 const today = new Date();
 function calculateAge(){
@@ -17,7 +18,7 @@ function calculateAge(){
         age = today.getFullYear() - birthDate.getFullYear() - 1;
     }
 
-    elResult.innerText = '귀하의 만 나이는 ${age} 입니다.';
+    elResult.innerText = `귀하의 만 나이는 ${age} 입니다.`;
 
     return age;
 
